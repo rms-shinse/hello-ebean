9ee0c4b3ecf88ec9a3437e0008280e68c8707295
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2020-05-08 16:21:09 +0900

    we can query by in instead of idIn
    https://github.com/ebean-orm/ebean/issues/1996#issuecomment-621066764

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("hello, world!");
     EbeanUtil.DatabaseFactory.create("default");
-    Archive.find.query().where().idIn(3393, 3309).asUpdate().set("data", 123).update();
-    Archive.find.query().where().idIn(3380, 3383).delete();
+    Archive.find.query().where().in("id", 3393, 3309).asUpdate().set("data", 123).update();
+    Archive.find.query().where().in("id", 3380, 3383).delete();
   }
 }
